c731a6cf4d9a5920bacf079ba7ea384b16230e13
Trio Saputra <triosaputra308@gmail.com>
2025-10-29 10:03:51 +0700

    update condition delete

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
 GRAPH_API_BASE = "https://graph.microsoft.com/v1.0"
-HOSTNAME = 'sqqmy.sharepoint.com'
-SITE_PATH = "TestISGS"
+HOSTNAME = '*'
+SITE_PATH = "*"
 
-TENANT_ID = "01b81770-4c98-4845-8ba0-151c7b22f724"
-CLIENT_ID = "f1579a3f-7ab3-4cbd-8236-eae27b8150ae"
-CLIENT_SECRET = "ivP8Q~ZUZsh0Q_xyJlFCb40KPQUT8tJC3i~2ccVT"
+TENANT_ID = "*"
+CLIENT_ID = "*"
+CLIENT_SECRET = "*"
 
 MINIO_URL = "http://127.0.0.1:9000"
 MINIO_ACCESSKEY = "minioadmin"
@@ -13,7 +13,7 @@ MINIO_SECRETKEY = "minioadmin"
 MINIO_BUCKET = "portal"
 
 
-# Spesify prefix folder in minio, exp: contract, contract/2024, portfolio
+# Spesify prefix folder in minio, exp: contract, contract/2024,portfolio
 PREFIX_FOLDER = [
     'contract',
     'portfolio'
